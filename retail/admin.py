0f5d623ee0f2a9402c2aedfe41861dbb20045ce4
DanStaf <danstaf@mail.ru>
2025-02-19 18:28:11 +0300

    admin configured

--- retail/admin.py
+++ retail/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-
 from retail.models import Product, Company
 
 admin.site.register(Product)
-admin.site.register(Company)
+
+
+@admin.action(description="Очистить задолженность")
+def clean_debt(modeladmin, request, queryset):
+    queryset.update(debt=0.0)
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'supplier',)
+    list_filter = ('city',)
+    actions = [clean_debt]
