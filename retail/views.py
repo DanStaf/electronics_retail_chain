7e4edfa0bb4f37cfb4163100ba2fe4edd0315f35
DanStaf <danstaf@mail.ru>
2025-02-19 18:09:20 +0300

    Dept is not allowed to change via API

--- retail/views.py
+++ retail/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ModelViewSet
 
 from retail.models import Company
@@ -9,3 +10,13 @@ class CompanyViewSet(ModelViewSet):
 
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+    def perform_update(self, serializer):
+
+        new_debt = serializer.validated_data.get("debt")
+
+        if new_debt or new_debt == 0:
+            raise ValidationError("'Debt' is not allowed to change")
+
+        new_item = serializer.save()
+        new_item.save()
